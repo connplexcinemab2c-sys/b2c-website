948a5bdfee53557a05a60c92a292ca7afca8f486
Your Name <youremail@example.com>
2026-09-17 16:04:27 +0530

    Revert ecommerce API endpoint back to shop-api.theconnplex.com

--- Admin_frontend/src/config/EcommerceService.js
+++ Admin_frontend/src/config/EcommerceService.js
@@ -4,13 +4,13 @@ const API_ENDPOINT =
   import.meta.env.VITE_ECOMMERCE_BASE_URL ||
   (typeof window !== "undefined" && (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://localhost:3067/api"
-    : "/ecommerce-api");
+    : "https://shop-api.theconnplex.com/api/");
 
 export const ECOMMERCE_IMAGES_API_ENDPOINT =
   import.meta.env.VITE_ECOMMERCE_IMAGE_URL ||
   (typeof window !== "undefined" && (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://localhost:3067/api/uploads"
-    : "/ecommerce-api/uploads");
+    : "https://shop-api.theconnplex.com/api/uploads");
 
 const EcommerceService = axios.create({
   baseURL: API_ENDPOINT,
